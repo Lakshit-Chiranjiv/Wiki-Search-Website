f1af73282c6db870acde513093e0f4002f80fd92
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-28 00:09:14 +0530

    navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="bg-slate-600 text-white/90 flex flex-col justify-around items-center md:items-center md:flex-row gap-4 sticky top-0 drop-shadow-lg p-4 py-8">
+      <Link href="/" className="font-bold text-3xl hover:text-white">Wiki Yard</Link>
+      <div className="flex gap-8 justify-center">
+        <Link href="/about" className="font-semibold text-xl hover:text-white">About</Link>
+        <Link href="/contact" className="font-semibold text-xl hover:text-white">Contact</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
