600b63e94d72a95214148fd0e830d87fad5a0d62
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-28 00:50:18 +0530

    added search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { Inter } from 'next/font/google'
+import Search from './components/Search'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="bg-red-400">
+    <main className="bg-slate-500 min-h-screen">
       Hello
+      <div className='w-full flex justify-center'>
+        <Search/>
+      </div>
     </main>
   )
 }
