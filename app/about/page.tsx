d2eb517a2df224fab262bf1f5403ac09e3dc2f49
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-28 23:30:18 +0530

    about page

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='text-white/80 text-xl'>
+        <h1 className='text-4xl font-bold my-4'>About Me</h1>
+        <p>My name is <strong className='font-semibold'>Lakshit Chiranjiv Sagar</strong> and I am a full stack web developer. There are other domains that I have worked on like <strong className='font-semibold'>Machine Learning</strong> , <strong className='font-semibold'>Data Science</strong> and <strong className='font-semibold'> Blockchain Development</strong> etc.</p>
+        <br />
+        <p>I am currently a student at <strong className='font-semibold'>Kalinga Institute of Industrial Technology</strong> pursuing my Bachelors in Computer Science and Engineering</p>
+        <br />
+        <p>I have a lot of experience working on <strong className='font-semibold'>React</strong> and <strong className='font-semibold'>Next.js</strong> and I am currently adapting on to NextJS 13. I have also worked on <strong className='font-semibold'>Node.js</strong> and <strong className='font-semibold'>Express.js</strong> for backend development. I have also worked on <strong className='font-semibold'>MongoDB</strong> and <strong className='font-semibold'>PostgreSQL</strong> for database management.</p>
+    </div>
+  )
+}
+
+export default About
