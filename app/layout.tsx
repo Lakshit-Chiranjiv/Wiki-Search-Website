540738c7d4d79ad3966c481a071d73e4617742dc
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-28 23:18:49 +0530

    bg and height

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className='bg-slate-500 min-h-screen'>
         <Navbar/>
         {children}
       </body>
