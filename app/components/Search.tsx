4a7f8553623e45d74cb7444eb5cfa91ef0f4ca7f
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-31 00:16:41 +0530

    search component functionalities and routing

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,10 +1,28 @@
-type Props = {}
+'use client'
 
-export default function Search({}: Props) {
+import { useState, MouseEvent } from "react"
+import { useRouter } from "next/navigation"
+
+export default function Search() {
+
+  const [search, setSearch] = useState('')
+
+  const router = useRouter()
+
+  const handleDump = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    router.push(`/yard/${search}`)
+    setSearch('')
+  }
+
   return (
     <div className="flex flex-col gap-4 md:flex-row text-sm w-fit items-center">
-        <input type="text" placeholder="Search Wikipedia" className="rounded p-2 outline-none w-56"/>
-        <button className="bg-slate-600 text-white/90 px-4 py-2 rounded-md hover:bg-slate-700 w-32">Dump🚀</button>
+        <input type="text" placeholder="Search Wikipedia" className="rounded p-2 outline-none w-56" onChange={(e) => {
+          setSearch(e.target.value)
+        }}
+        value={search}
+        />
+        <button className="bg-slate-600 text-white/90 px-4 py-2 rounded-md hover:bg-slate-700 w-32" onClick={handleDump}>Dump🚀</button>
     </div>
   )
 }
