f02508c7230b5c7b3bdd0b822a4877689ce062a2
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-03-30 16:50:38 +0530

    rendered hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
-import { Inter } from 'next/font/google'
-import Search from './components/Search'
-
-const inter = Inter({ subsets: ['latin'] })
+import Hero from './components/Hero'
 
 export default function Home() {
+
   return (
     <main>
-      <div className='w-full flex justify-center my-8'>
-        <Search/>
-      </div>
+      <Hero/>
     </main>
   )
 }
