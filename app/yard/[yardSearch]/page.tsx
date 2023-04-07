444bc5ca7dc24860c2c9f5c617c9c5072bfaebb6
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-07 17:02:21 +0530

    fetching and result display

--- app/yard/[yardSearch]/page.tsx
+++ app/yard/[yardSearch]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import getWikiData from '@/lib/getWikiData'
 
 type Props = {
   params: {
@@ -6,10 +7,27 @@ type Props = {
   }
 }
 
-export default function YardSearch({ params: { yardSearch } }: Props) {
+export default async function YardSearch({ params: { yardSearch } }: Props) {
+
+  const wikiData: Promise<SearchResult> = getWikiData(yardSearch)
+  const wikiDataJson = await wikiData
+  const results: Result[] | undefined = wikiDataJson?.query?.pages
+  if(!results) return <p>No results found</p>
+  const resultsArray = Object.values(results)
+  console.log(results,"results")
+
   return (
     <div>
-      <h1 className='text-6xl'>{yardSearch}</h1>
+      {
+        resultsArray.map((result: Result) => {
+          return (
+            <div key={result.pageid}>
+              <h1>{result.title}</h1>
+              <p>{result.extract}</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
