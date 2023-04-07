8fd25a9f7f72884e101ac915c85a9dbc5b7c9762
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-08 00:19:09 +0530

    centered button

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,9 +8,11 @@ const Hero = () => {
   return (
     <div className='md:w-3/4 mx-auto md:flex md:flex-col md:gap-8 md:items-center font-poppins'>
         <h1 className='text-xl font-bold text-white/90 md:text-4xl lg:text-6xl my-12'>Dump out with us and know whatever you want</h1>
-        <button className='bg-white rounded p-2 md:px-8 md:py-4 hover:bg-white/90 active:scale-95 font-semibold' onClick={() => {
-        router.push('/yard')
-        }}>Head to the Yard</button>
+        <div className='flex justify-center'>
+          <button className='bg-white rounded p-2 md:px-8 md:py-4 hover:bg-white/90 active:scale-95 font-semibold' onClick={() => {
+          router.push('/yard')
+          }}>Head to the Yard</button>
+        </div>
     </div>
   )
 }
