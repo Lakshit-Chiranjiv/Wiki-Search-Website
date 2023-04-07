d0d05b8e7a1b7e16eec04f1023d426eb27a3d0e8
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-07 23:56:13 +0530

    loading component

---
+++ app/yard/[yardSearch]/loading.tsx
@@ -0,0 +1,9 @@
+const LoadingSearch = () => {
+  return (
+    <div className="bg-white/90 font-poppins p-4 py-8 flex flex-col gap-8 justify-center items-center rounded shadow-md mx-auto my-8">
+        <h2 className="text-4xl text-yellow-400 font-semibold">Loading...</h2>
+    </div>
+  )
+}
+
+export default LoadingSearch
