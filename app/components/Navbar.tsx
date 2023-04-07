a17cac8007b449c929d493282fddec7709820d8c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-07 23:22:47 +0530

    poppins font

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Navbar = (props: Props) => {
   return (
-    <div className="bg-slate-600 text-white/90 flex flex-col justify-around items-center md:items-center md:flex-row gap-4 sticky top-0 drop-shadow-lg p-4 py-8">
+    <div className="bg-slate-600 text-white/90 flex flex-col justify-around items-center md:items-center md:flex-row gap-4 sticky top-0 drop-shadow-lg p-4 py-8 font-poppins">
       <Link href="/" className="font-bold text-3xl hover:text-white">Wiki Yard</Link>
       <div className="flex gap-8 justify-center">
         <Link href="/about" className="font-semibold text-xl hover:text-white">About</Link>
