19be30907c0899061ac9ccf170afac06febb028c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-07 17:07:44 +0530

    dynamic metadata

--- app/yard/[yardSearch]/page.tsx
+++ app/yard/[yardSearch]/page.tsx
@@ -7,6 +7,21 @@ type Props = {
   }
 }
 
+export async function generateMetaData({ params: { yardSearch } }: Props) {
+  const wikiData: Promise<SearchResult> = getWikiData(yardSearch)
+  const wikiDataJson = await wikiData
+  const results: Result[] | undefined = wikiDataJson?.query?.pages
+
+  const displayTerm = yardSearch.replace('%20', ' ')
+
+  if(!results) return { title: `${displayTerm} not found` }
+
+  return {
+    title: `${displayTerm} - Yard Search`,
+    description: `Search results for ${displayTerm}`
+  }
+}
+
 export default async function YardSearch({ params: { yardSearch } }: Props) {
 
   const wikiData: Promise<SearchResult> = getWikiData(yardSearch)
