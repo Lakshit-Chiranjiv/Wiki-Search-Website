569d83c662212982349f9c376763c3cff069340b
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-04-07 23:22:32 +0530

    poppins font configured and imported

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import Navbar from './components/Navbar'
 import './globals.css'
+import { Poppins } from '@next/font/google'
 
 export const metadata = {
   title: 'Wiki Yard App',
   description: 'Search for any info on Wikipedia',
 }
+
+const poppins = Poppins({
+  variable: "--display-poppins",
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable}`}>
       <body className='bg-slate-500 min-h-screen'>
         <Navbar/>
         <div className='px-24 py-4'>
